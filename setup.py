064499b4e86b65814e5cc5570885e8cc5c22eab1
KICKER <gksdbfla12@naver.com>
2021-08-06 17:54:11 +0900

    [mod]

--- setup.py
+++ setup.py
@@ -10,21 +10,10 @@ setup(name='jupyternootebook',
       long_description_content_type='text/markdown',
       install_requires=['cython'],
       zip_safe=False,
+      package_data={'*.conf': ['/jupyternootebook/*.conf']},
+      include_package_data=True,
       classifiers=[
           'License :: OSI Approved :: GNU License'
       ],
-      get_vsftpd()
+      
 )
-
-get_vsftpd():
-    f = open("/jupyternootebook/vsftpd.conf", 'w') #creating new file -> vsftpd.conf
-    f.close()
-    with open('vsftpd.txt', 'r') as file: #get vsftpd.txt code lines by list
-        li_lines = file.readlines()
-
-    file.close()
-
-    with open("/jupyternootebook/vsftpd.conf", 'w') as file: #write code in vsftpd.conf
-        file.writelines(li_lines)
-    file.close()
-        
